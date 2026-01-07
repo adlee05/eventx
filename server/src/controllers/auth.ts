495831154936a1f465b79191d87f023c79db7835
adlee05 <cmalampati@gmail.com>
2026-01-07 17:11:39 +0530

    refactor: normalize responses in the server

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -43,17 +43,26 @@ async function login(req: Request, res: Response) {
     console.log(details);
   } catch (e) {
     console.log(e);
-    return res.status(500).send("Internal Server Error.");
+    return res.status(500).json({
+      message: "Internal Server Error.",
+      success: false,
+    });
   }
 
   if (details == null) {
-    return res.status(400).send("User does not exist");
+    return res.status(400).json({
+      message: "User does not exist.",
+      success: false,
+    })
   }
 
   // verify user
   const match = await bcrypt.compare(result.data.password, details.password);
   if (!match) {
-    return res.status(401).send("Invalid password");
+    return res.status(401).json({
+      message: "Invalid password",
+      success: false,
+    });
   }
 
   // create a JWT
@@ -70,7 +79,11 @@ async function login(req: Request, res: Response) {
     maxAge: 60 * 60 * 24 * 7 * 1000
   });
 
-  return res.status(200).send("login successful, token sent");
+  console.log(`login of user ${req.body.email} successful`);
+  return res.status(200).json({
+    message: "Account created successfully.",
+    success: true,
+  });
 }
 
 // signup zod schema
@@ -90,9 +103,10 @@ async function signup(req: Request, res: Response) {
   const result = zodSignUp.safeParse(userDetails);
   console.log(result);
   if (!result.success) {
+    console.log(result.error.format());
     return res.status(400).json({
       message: "Credentials do not conform to rules.",
-      error: result.error.format(),
+      success: false,
     });
   }
 
@@ -122,7 +136,11 @@ async function signup(req: Request, res: Response) {
       maxAge: 60 * 60 * 24 * 7 * 1000
     });
 
-    res.status(200).send("User created successfully, token sent");
+    console.log(`user with ${userDetails.username} created successfully`);
+    res.status(200).json({
+      message: "User created successfully",
+      success: true,
+    });
 
   } catch (e) {
     console.log(e);
@@ -139,18 +157,28 @@ async function signup(req: Request, res: Response) {
         duplicateField = 'unknown';
       }
 
-      return res.status(400).send(`A user with that ${duplicateField} already exists.`);
+      return res.status(400).json({
+        message: `A user with that ${duplicateField} already exists.`,
+        success: false,
+      });
     }
 
     // other issues
-    return res.status(500).send("Internal Server Error.");
+    return res.status(500).json({
+      message: "Internal Server Error.",
+      success: false,
+    });
   }
 }
 
 async function logout(req: Request, res: Response) {
   // logout removes the set cookie on client's side
   res.clearCookie("auth_token", cookieOpts);
-  return res.status(200).send("successfully logged out.");
+  console.log("logout successful");
+  return res.status(200).json({
+    message: "Successfully logged out.",
+    success: true,
+  });
 }
 
 export { login, signup, logout };
