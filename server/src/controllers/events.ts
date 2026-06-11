eb6a437ac6fc566d0a0a72be4c7ccad8d875a871
adlee05 <cmalampati@gmail.com>
2026-06-11 19:32:32 +0530

    feat: preliminary validation for addEvent route, remove other route controllers

--- server/src/controllers/events.ts
+++ server/src/controllers/events.ts
@@ -1,70 +1,74 @@
 import { Request, Response } from "express";
 import { EventModel } from "../models/event.js";
-import axios from "axios";
+import { formDataSer } from "../types/formData.js";
+import { UserModel } from "../models/user.js";
 
 // add a new event
 async function addEvent(req: Request, res: Response) {
-  try {
-    const { title, description, startTime, endTime, imageUrl, location,
-      status, category } = req.body;
+  const data: formDataSer = req.body.data;
 
-    if (!title || !description || !startTime || !endTime || !location) {
-      return res.status(400).json({
-        message: "Must fill all required fields!",
-        success: false
-      });
-    }
+  // validation
 
-    //@ts-ignore
-    const userId = req.user.userId;
-
-    const event = await EventModel.create({
-      title,
-      category,
-      startTime,
-      endTime,
-      description,
-      location,
-      imageUrl,
-      status,
-      createdBy: userId,
-      registeredUsers: [],
-    });
-
-    return res.status(201).json({
-      message: "Event added successfully!",
-      event,
-      success: false
-    });
-
-  } catch (error) {
-    console.error("Error adding Event!", error);
-    return res.status(500).json({
-      message: "Failed to add Event!",
-      success: false
+  // validate title, desc and urls
+  if (
+    data.title.trim().length > 50 ||
+    data.desc.trim().length > 200 ||
+    data.imgurl.trim().length > 300 ||
+    data.location.trim().length > 300
+  ) {
+    return res.json({
+      success: false,
+      message: "Invalid length of form values, please check the length of either title, desc, imgurl or location",
     });
   }
 
-}
-
-// Get events created by a user
-async function userEvents(req: Request, res: Response) {
-  try {
-    const userId = req.user.userId;
-
-    const events = await EventModel.find({ userId }).sort({ date: -1 });
-
-    res.json({
-      message: " Events fetched Sucessfully!",
-      data: events
-    });
-
-  } catch (error) {
-    console.error("Error fetching Events!", error);
-    res.status(500).json({
-      message: "Failed to fetch Events!",
+  // validate maxnums
+  if (data.maxnums <= 0 || data.maxnums > 100) {
+    return res.json({
+      success: false,
+      message: "Invalid value for maxnums",
     });
   }
+
+  if (!["recreational", "tech", "art"].includes(data.category)) {
+    return res.json({
+      success: false,
+      message: "Invalid category type"
+    });
+  }
+
+  // validate dates
+  const now = new Date();
+
+  const startDate = (
+    data.startDate instanceof Date ? data.startDate : new Date(data.startDate)
+  );
+
+  const deadDate = (
+    data.deadDate instanceof Date ? data.deadDate : new Date(data.deadDate)
+  );
+
+  if (startDate <= now || deadDate <= now) {
+    return res.json({
+      success: false,
+      message: "Event cannot start in the past",
+    })
+  }
+
+  if (startDate <= deadDate) {
+    return res.json({
+      success: false,
+      message: "Start time should be strictly after the deadline time",
+    })
+  }
+
+  // validate user
+  const userExists = await UserModel.exists({ username: data.createdBy });
+
+  if (!userExists) return res.json({
+    success: false,
+    message: "User does not exist",
+  })
 }
 
 // get all available events
@@ -118,75 +122,4 @@ async function eventById(req: Request, res: Response) {
   }
 }
 
-// delete a particular event
-async function deleteEventById(req: Request, res: Response) {
-  try {
-
-    const { id } = req.params;
-
-    const userId = req.user.userId;
-
-    const event = await EventModel.findOneAndDelete({ _id: id, userId })
-
-    res.status(200).json({
-      message: " Event Deleted SucessFully!"
-    })
-
-  } catch (error) {
-
-    console.error("Error Deleting all events!", error);
-    return res.status(500).json({
-      message: "Failed to Delete events!",
-    });
-
-  }
-}
-
-async function changeEventDetails(req: Request, res: Response) {
-  try {
-
-    const { id } = req.params;
-    const { title, description, date, imageUrl, location,
-      certificateText, certificateTemplateUrl, status } = req.body;
-
-    //@ts-ignore
-    const userId = req.user.userId;
-
-    const event = await EventModel.findOne({ _id: id, userId });
-
-    if (!event) {
-      res.status(404).json({
-        message: "Event Not Found!"
-      })
-    }
-
-    if (event) {
-      if (title) event.title = title;
-      if (description) event.description = description;
-      if (date) event.date = date;
-      if (imageUrl) event.imageUrl = imageUrl;
-      if (location) event.location = location;
-      if (certificateText) event.certificateText = certificateText;
-      if (certificateTemplateUrl) event.certificateTemplateUrl = certificateTemplateUrl;
-      if (status) event.status = status;
-
-    }
-
-    await event?.save();
-
-    res.json({
-      message: "Event Updated Sucessfully!",
-      data: event
-    });
-
-
-  } catch (error) {
-    console.error("Error fetching all events!", error);
-    return res.status(500).json({
-      message: "Failed to fetch events!",
-    });
-
-  }
-}
-
-export { addEvent, getAllEvents, changeEventDetails, deleteEventById, userEvents, eventById };
+export { addEvent, getAllEvents, eventById };
