bb3392ba5c157e992d9ac4a990dcdb0302c89392
adlee05 <cmalampati@gmail.com>
2026-06-11 19:31:21 +0530

    fix: streamline event model, remove incomplete routes

--- server/src/models/event.ts
+++ server/src/models/event.ts
@@ -5,19 +5,14 @@ const EventSchema = new Schema(
     title: { type: String, required: true },
     description: { type: String },
     startTime: { type: Date, required: true },
-    endTime: { type: Date, required: true },
     location: { type: String, required: true },
     imageUrl: { type: String },
     createdBy: { type: String },
     registeredUsers: [{ type: Schema.Types.ObjectId, ref: "User", },],
     attendees: [{ type: Schema.Types.ObjectId, ref: "User", },],
-    certificateTemplateUrl: { type: String },
     maxParticipants: { type: Number },
-    registrationDeadline: { type: Date },
-
-    status: { type: String, enum: ["upcoming", "completed", "cancelled"], default: "upcoming", },
-    category: {type: String, enum: ["recreational", "tech", "art"], default: "recreational"},
-    certificateText: { type: String },
+    deadTime: { type: Date },
+    category: { type: String, enum: ["recreational", "tech", "art"], default: "recreational" },
   },
   { timestamps: true }
 );
--- server/src/routes/events.ts
+++ server/src/routes/events.ts
@@ -3,19 +3,13 @@ import authenticate from "../middleware/authenticate.js";
 import { EventModel } from "../models/event.js";
 import { addEvent, 
   getAllEvents, 
-  changeEventDetails, 
-  deleteEventById, 
-  userEvents, 
   eventById 
 } from "../controllers/events.js";
 
 const router = express.Router();
 
 router.post("/addEvent", authenticate, addEvent);
-router.get("/userEvents", authenticate, userEvents);
 router.get("/allEvents", getAllEvents);
 router.get("/:id", eventById);
-router.put("/:id", authenticate, changeEventDetails);
-router.delete("/event/:id", authenticate, deleteEventById);
 
 export default router;
