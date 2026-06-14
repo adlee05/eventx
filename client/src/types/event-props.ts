1d28f7ab344bb146522a4f6be0d517dc066f1aad
adlee05 <cmalampati@gmail.com>
2026-06-14 21:00:16 +0530

    fix: update event-props type according to new schema

--- client/src/types/event-props.ts
+++ client/src/types/event-props.ts
@@ -1,12 +1,17 @@
 type EventProps = {
   category: string,
   _id: string,
-  startTime: Date,
+  startDate: Date,
+  startTime: string,
   duration: string,
   title: string,
   description: string,
   location: string,
-  imageUrl: string
+  imageUrl: string,
+  deadTime: string,
+  deadDate: Date,
+  createdBy: string,
+  maxParticipants: string
 };
 
 export type { EventProps };
